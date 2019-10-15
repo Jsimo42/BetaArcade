53bf9870dea8954ca07e7199ce02f8c84f0c5ceb
ABirch98 <T7088521@live.tees.ac.uk>
2019-10-15 10:59:57 +0100

    V1.4 Added TestItem
    Added collectable item

---
+++ MasterBuild/Source/MasterBuild/PickUps/TestItem.cpp
@@ -0,0 +1,47 @@
+//@AdamBirch 15/10/19
+
+#include "TestItem.h"
+
+
+// Sets default values
+ATestItem::ATestItem()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ItemMesh"));
+	RootComponent = Mesh;
+
+
+}
+
+// Called when the game starts or when spawned
+void ATestItem::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATestItem::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	Float(DeltaTime);
+
+}
+
+void ATestItem::Float(float DeltaTime) //while item is alive moves up and down and (TODO: rotates)
+{
+	UPROPERTY(VisableAnywhere, Category = Movement)
+	FVector CurrentLocation;
+	CurrentLocation = this->GetActorLocation();
+	if (CurrentLocation.Z > MaxHeight)//TODO: set as an offset from minheight 
+	{
+		speed = -speed;
+	}
+	if (CurrentLocation.Z < MinHeight)//TODO: set as spawn height
+	{
+		speed = -speed;
+	}
+	SetActorLocation(FVector(CurrentLocation.X, CurrentLocation.Y, (CurrentLocation.Z + (speed*DeltaTime))));
+}
