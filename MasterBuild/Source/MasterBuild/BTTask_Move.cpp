b7030476990032cc3c23634258a406e21c45b57b
Jake Simpson <jsimo42@gmail.com>
2019-10-22 09:26:42 +0100

    Revert "Revert "AI movement and fear""
    This reverts commit 75314a522db992c2d1a4320f11ff5fdb747f4a62.

--- MasterBuild/Source/MasterBuild/BTTask_Move.cpp
+++ MasterBuild/Source/MasterBuild/BTTask_Move.cpp
@@ -8,6 +8,8 @@
 #include "Sheep/SheepAI.h"
 #include "Sheep/SheepCharacter.h"
 #include "Player/Char_Wolf.h"
+#include "NavigationSystem.h"
+#include "Engine.h"
 
 EBTNodeResult::Type UBTTask_Move::ExecuteTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory)
 {
@@ -15,9 +17,42 @@ EBTNodeResult::Type UBTTask_Move::ExecuteTask(UBehaviorTreeComponent & OwnerComp
 
 	AChar_Wolf *Wolf = Cast<AChar_Wolf>(OwnerComp.GetBlackboardComponent()->GetValue<UBlackboardKeyType_Object>(SheepPC->WolfKeyID));
 
+
 	if (Wolf)
 	{
-		SheepPC->MoveToActor(Wolf, 5.f, true, true, true, 0, true);
+		//SheepPC->MoveToActor(Wolf, 5.f, true, true, true, 0, true);
+		//SheepPC->MoveToLocation(FVector(-970, -345, 202), 5.f, true, true, true, true, 0, true);
+		
+		//GEngine->AddOnScreenDebugMessage(-1, 15.f, FColor::Green, FString::Printf(TEXT("distance = %f"), FVector::Dist(SheepPC->GetPawn()->GetActorLocation(), Wolf->GetActorLocation())));
+
+		if (FVector::Dist(SheepPC->GetPawn()->GetActorLocation(), Wolf->GetActorLocation()) <= 300.f)
+		{
+			FVector movement = SheepPC->GetPawn()->GetActorLocation() - Wolf->GetActorLocation();
+			movement.Normalize(1.f);
+
+			movement = movement * 150;
+
+			FVector dest = SheepPC->GetPawn()->GetActorLocation() + movement;
+			
+			//GEngine->AddOnScreenDebugMessage(-1, 15.f, FColor::Green, FString::Printf(TEXT("working , vector to move = %f %f %f"), dest.X, dest.Y, dest.Z));
+
+			SheepPC->MoveToLocation(dest, 5.f, true, true, true, true, 0, true);
+		}
+
+		else if (SheepPC->GetMoveStatus() == EPathFollowingStatus::Idle)
+		{
+			FNavLocation RandomLocation;
+
+			UNavigationSystemV1 *NavSystem = UNavigationSystemV1::GetCurrent(GetWorld());
+
+			NavSystem->GetRandomReachablePointInRadius(SheepPC->GetPawn()->GetActorLocation(), 500.f, RandomLocation, NavSystem->GetDefaultNavDataInstance());
+
+			SheepPC->MoveToLocation(RandomLocation.Location, 5.f, true, true, true, true, 0, true);
+			//GEngine->AddOnScreenDebugMessage(-1, 15.f, FColor::Green, FString::Printf(TEXT("location tp go to : x = %f y = %f z = %f"), RandomLocation.Location.X, RandomLocation.Location.Y, RandomLocation.Location.Z));
+		}
+
+		
+
 		return EBTNodeResult::Succeeded;
 	}
 	else
